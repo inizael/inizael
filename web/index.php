0c6ad2f5580276e096b2635fdd7ab17718b3f492
Isael Caballero <fcaballero@relappro.com>
2018-11-12 08:41:15 -0600

    Se agregaron archivos de estilo, como fuentes, y archivos CSS.

--- web/index.php
+++ web/index.php
@@ -4,10 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/design.css">
+    <link rel="stylesheet" href="css/design.css"> <!-- DEsign -->
+    <link rel="stylesheet" href="css/design.mobile.css"> <!-- DEsign -->
+    <link rel="stylesheet" href="css/fontstyle.css"> <!-- Fonts -->
+    <link rel="stylesheet" href="fonts/material/style.css"><!-- Meterial Font -->
     <title>station x culture</title>
 </head>
 <body>
-    
+    <div id="views">
+        <div id="index">
+        </div>
+        <div id="user">
+        </div>
+        <div id="podcast">
+        </div>
+    </div>
+    <div id="navbar">
+        <ul class="navbar">
+            <li>
+                <span class="sxc_logo white nav">
+                </span>
+            </li>
+            <li title="Todos los Podcast">
+                <i class="icon-surround_sound"></i>
+            </li>
+            <li title="Todas las Estaciones">
+                <i class="icon-radio"></i>
+            </li>
+            <li title="Historial">
+                <i class="icon-person"></i>
+            </li>
+        </ul>
+    </div>
 </body>
 </html>
