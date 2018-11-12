df72443941a9e36a2edb09d6b455dd03a1277d68
Isael Caballero <fcaballero@relappro.com>
2018-11-12 15:47:46 -0600

    Se agregaron contenidos para la creacion del site

--- index.php
+++ index.php
@@ -4,19 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/design.css"> <!-- DEsign -->
-    <link rel="stylesheet" href="css/design.mobile.css"> <!-- DEsign -->
-    <link rel="stylesheet" href="css/fontstyle.css"> <!-- Fonts -->
-    <link rel="stylesheet" href="fonts/material/style.css"><!-- Meterial Font -->
+    <link rel="stylesheet" href="web/css/design.css"> <!-- DESIGN -->
+    <link rel="stylesheet" href="web/css/design.mobile.css"> <!-- DESIGN MOBILE-->
+    <link rel="stylesheet" href="web/css/design.grid.css"> <!-- DESIGN GRID-->
+    <link rel="stylesheet" href="web/css/fontstyle.css"> <!-- FUENTES -->
+    <link rel="stylesheet" href="web/fonts/material/style.css"><!-- ICONOS-->
     <title>station x culture</title>
 </head>
 <body>
     <div id="views">
-        <div id="index">
+        <div id="index" class="view visible">
+            <div class="container">
+                <div class="line">
+                    <div class="col-12">
+                        <h2 class="title">Ultimo Agregado</h2>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div id="user">
+        <div id="user" class="view hide">
         </div>
-        <div id="podcast">
+        <div id="podcast" class="view hide">
         </div>
     </div>
     <div id="navbar">
